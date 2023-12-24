1cadd32d729794917a79ac7fec2b067a2005ad9f
Abraao4raujo <a68816619@gmail.com>
2023-12-24 16:00:05 -0300

    adicionando modal criar sala

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,8 +3,13 @@ import "../styles/main.css";
 import HorizontalCards from "../components/cards/HorizontalCards.jsx";
 import useFetch from "../adapters/useFetch.jsx";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 const Home = () => {
+  const [showModal, setShowModal] = useState(false);
+  const [salaCriada, setSalaCriada] = useState(false);
+  const [nomeSala, setNomeSala] = useState("");
+
   const data = useFetch(
     `https://api.themoviedb.org/3/movie/popular?api_key=${
       import.meta.env.VITE_API_KEY
@@ -13,7 +18,44 @@ const Home = () => {
 
   return (
     <>
-      <Header />
+      <Header
+        setShowModal={setShowModal}
+        showModal={showModal}
+        salaCriada={salaCriada}
+        nomeSala={nomeSala}
+      />
+      {showModal && (
+        <div className="modalContainer">
+          <div className="headerModal">
+            <h2>Criar Sala</h2>
+          </div>
+          <div className="mainModal">
+            <div className="inputsName">
+              <label htmlFor="nome">Nome da sala</label>
+              <input
+                type="text"
+                name="nome"
+                id="mainNome"
+                onChange={(event) => setNomeSala(event.target.value)}
+              />
+            </div>
+            <div className="inputsPassword">
+              <label htmlFor="password">Senha da sala</label>
+              <input type="text" name="password" id="mainPassword" />
+            </div>
+          </div>
+          <div className="footerModal">
+            <button
+              onClick={() => {
+                setSalaCriada(true);
+                setShowModal(false);
+              }}
+            >
+              Criar sala
+            </button>
+          </div>
+        </div>
+      )}
       <div className="containerFilmeDestaque">
         <img src="../../public/img/capa-zootopia.png" />
       </div>
