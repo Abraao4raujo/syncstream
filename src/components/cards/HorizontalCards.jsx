befaa434e0dd2927ebb0ae84e988288e281d1ab5
Abraao4raujo <a68816619@gmail.com>
2023-12-24 14:44:17 -0300

    fix card logic

--- src/components/cards/HorizontalCards.jsx
+++ src/components/cards/HorizontalCards.jsx
@@ -1,38 +1,39 @@
 import Card from "../card/card.jsx";
 import "../../styles/cards.css";
-import "../card/teste.js";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const HorizontalCards = (dataAPI) => {
-  const [number, setNumber] = useState(1);
+  const [number, setNumber] = useState(0);
 
-  function changeSlide() {
+  function changeSlide(newNumber) {
     let listaFilmes = document.querySelector(".cards");
 
-    listaFilmes.scrollLeft = 290 * number;
+    console.log(newNumber);
+
+    if (number >= 0) {
+      listaFilmes.scrollLeft = 290 * number;
+    }
   }
+
+  function decreaseNumber() {
+    setNumber((prevNumber) => prevNumber - 1);
+  }
+
+  function increaseNumber() {
+    setNumber((prevNumber) => prevNumber + 1);
+  }
+
+  useEffect(() => {
+    changeSlide(number);
+  }, [number]);
 
   // const tamanhoTotalTela = 4350;
 
   return (
     <div className="container-cards">
       <ul className="movie-list">
-        <li
-          onClick={() => {
-            setNumber(number - 1);
-            changeSlide(number);
-          }}
-        >
-          {"<"}
-        </li>
-        <li
-          onClick={() => {
-            setNumber(number + 1);
-            changeSlide(number);
-          }}
-        >
-          {">"}
-        </li>
+        <li onClick={decreaseNumber}>{"<"}</li>
+        <li onClick={increaseNumber}>{">"}</li>
       </ul>
 
       <ul className="cards">
