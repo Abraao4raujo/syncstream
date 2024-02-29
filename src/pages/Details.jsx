8a27a1108b2de75bbcad9acab8753f709ae1ff97
Abraao4raujo <a68816619@gmail.com>
2024-02-29 13:26:36 -0300

    responsive-mobile

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -1,28 +1,28 @@
+import { useEffect, useState } from "react";
 import "../styles/details.css";
 
 const Details = ({ infApi, setConteudo }) => {
   return (
-    <>
-      <div className="details-body-content">
-        <div className="details-content">
-          <div className="details-container-img">
-            <img src={infApi.img} alt="" />
-          </div>
-          <div className="details-text">
-            <h2 className="details-title">{infApi.title || infApi.name}</h2>
-            <p className="details-text-paragraph">{infApi.desc}</p>
-          </div>
-          <span
-            className="details-btn-close-modal"
-            onClick={() => {
-              setConteudo(null);
-            }}
-          >
-            X
-          </span>
+    <div
+      className="details-body-content"
+      onClick={(e) => {
+        console.log(e.target.className);
+        if (e.target.className === "details-content") setConteudo(null);
+      }}
+    >
+      <div className="details-content">
+        <div className="details-container-img">
+          <img
+            src={window.innerWidth < "750" ? infApi.backdropPath : infApi.img}
+            alt=""
+          />
+        </div>
+        <div className="details-text">
+          <h2 className="details-title">{infApi.title || infApi.name}</h2>
+          <p className="details-text-paragraph">{infApi.desc}</p>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
