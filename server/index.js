5b5908cf79a5c00fc944b2c2abbfde373bbd1705
Abraao4raujo <a68816619@gmail.com>
2024-03-06 11:30:47 -0300

    add server

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "https://syncstream-three.vercel.app/home",
+    origin: "http://localhost:5173",
     methods: ["GET", "POST"],
   },
 });
